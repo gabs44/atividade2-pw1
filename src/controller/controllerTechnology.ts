4ded8807ebf4e86efc0aaa736f2ac7980601a4d4
gabriella <gabriellabraga.academico@gmail.com>
2023-11-16 12:44:22 -0300

    Ajusta formatação do código

--- src/controller/controllerTechnology.ts
+++ src/controller/controllerTechnology.ts
@@ -1,64 +1,75 @@
 import { Request, Response } from "express";
 import { prisma } from "../database/repositoryClient";
 import { CustomRequest } from "../interfaces/CustomRequest";
-import {ServiceTechnology } from "../service/serviceTechnology";
+import { ServiceTechnology } from "../service/serviceTechnology";
 import { User } from "@prisma/client";
 import { TechnologyError } from "../utils/errors";
 
-const serviceTechnology = new ServiceTechnology()
+const serviceTechnology = new ServiceTechnology();
 
-export class ControllerTechnology{
-  async list(req: CustomRequest, res: Response){
+export class ControllerTechnology {
+  async list(req: CustomRequest, res: Response) {
     const user = req.user?.id!;
-    try{
+    try {
       const ocorrencias = await serviceTechnology.list(user);
-      res.json(ocorrencias)
-    }catch(error:unknown){
-      res.status(404).json(error)
-    }
-  }async create(req: CustomRequest, res: Response){
-    const {title, deadline} = req.body;
-    const user = req.user!;
-    try{
-      const technology = await serviceTechnology.create({title, deadline, user})
-      res.status(201).json(technology)
-    }catch(error:unknown){
-      if(error instanceof TechnologyError){
-        res.status(404).json({error: error.message})
-      }
-      else{
-        res.status(404).json({error: 'Somethigh went wrong'})
-      }
-    }
-  }async update(req: CustomRequest, res: Response){
-    const user = req.user!
-    const {id} = req.params
-    const {title, deadline} = req.body;
-    try{
-      const technology = await serviceTechnology.update({id, title, deadline, user})
-      res.status(200).json(technology)
-    }catch(error: unknown){
-      res.status(404).json({error: "Technology not found"})
-    }
-  } async updateMany(req:CustomRequest, res: Response){
-    const userId = req.user!.id
-    const {id} = req.params
-    try{
-       await serviceTechnology.updateMany({userId, id})
-      res.status(200).json({ message: "Technology updated successfully" })
-    }catch(error: unknown){
-      res.status(404).json({error: "Technology not found"})
+      res.json(ocorrencias);
+    } catch (error: unknown) {
+      res.status(404).json(error);
     }
   }
-  async delete(req: CustomRequest, res: Response){
-    const userId = req.user!.id
-    const {id} = req.params
-    try{
-      await serviceTechnology.delete({userId, id})
-      const listUpdate = await serviceTechnology.list(userId)
-      res.status(200).json({listUpdate})
-    }catch(error:unknown){
-      res.status(404).json({error: "Technology not found"})
+  async create(req: CustomRequest, res: Response) {
+    const { title, deadline } = req.body;
+    const user = req.user!;
+    try {
+      const technology = await serviceTechnology.create({
+        title,
+        deadline,
+        user,
+      });
+      res.status(201).json(technology);
+    } catch (error: unknown) {
+      if (error instanceof TechnologyError) {
+        res.status(404).json({ error: error.message });
+      } else {
+        res.status(404).json({ error: "Somethigh went wrong" });
+      }
+    }
+  }
+  async update(req: CustomRequest, res: Response) {
+    const user = req.user!;
+    const { id } = req.params;
+    const { title, deadline } = req.body;
+    try {
+      const technology = await serviceTechnology.update({
+        id,
+        title,
+        deadline,
+        user,
+      });
+      res.status(200).json(technology);
+    } catch (error: unknown) {
+      res.status(404).json({ error: "Technology not found" });
+    }
+  }
+  async updateMany(req: CustomRequest, res: Response) {
+    const userId = req.user!.id;
+    const { id } = req.params;
+    try {
+      await serviceTechnology.updateMany({ userId, id });
+      res.status(200).json({ message: "Technology updated successfully" });
+    } catch (error: unknown) {
+      res.status(404).json({ error: "Technology not found" });
+    }
+  }
+  async delete(req: CustomRequest, res: Response) {
+    const userId = req.user!.id;
+    const { id } = req.params;
+    try {
+      await serviceTechnology.delete({ userId, id });
+      const listUpdate = await serviceTechnology.list(userId);
+      res.status(200).json({ listUpdate });
+    } catch (error: unknown) {
+      res.status(404).json({ error: "Technology not found" });
     }
   }
 }
--- src/service/serviceTechnology.ts
+++ src/service/serviceTechnology.ts
@@ -5,122 +5,127 @@ import { UUID } from "crypto";
 import { User } from "@prisma/client";
 import { TechnologyError } from "../utils/errors";
 
-type Params ={
-  title:string;
-  deadline: Date
-  user: User
-  id?: string
-}
+type Params = {
+  title: string;
+  deadline: Date;
+  user: User;
+  id?: string;
+};
 
 type ParamsUserTech = {
   userId: string;
-  id: string
-}
+  id: string;
+};
 
-export class ServiceTechnology{
+export class ServiceTechnology {
   async list(id: string) {
-    try{
+    try {
       const technologies = await prisma.technology.findMany({
-        where:{
-          userId:id
-        }
-      }) 
+        where: {
+          userId: id,
+        },
+      });
       return technologies;
-    } catch (err){
-        console.log(err)
-        return []
-      }
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
   }
-  async create({title, deadline, user}: Params){
+  async create({ title, deadline, user }: Params) {
     try {
       const existingTechnology = await prisma.technology.findFirst({
-        where:{
-          title: title != undefined ? title: "",
-          userId: user.id
+        where: {
+          title: title != undefined ? title : "",
+          userId: user.id,
         },
-      });console.log(existingTechnology)
+      });
       if (existingTechnology) {
-        throw new TechnologyError('Technology already exists');
+        throw new TechnologyError("Technology already exists");
       }
       const createTechnology = await prisma.technology.create({
-        data:{
+        data: {
           title,
-          studied: false, 
+          studied: false,
           deadline: new Date(deadline),
           created_at: new Date(),
           User: {
             connect: { id: user.id },
-          }
-        }
-      })
-          return createTechnology
-    }catch(err){
-      if(err instanceof TechnologyError){
-        throw err
+          },
+        },
+      });
+      return createTechnology;
+    } catch (err) {
+      if (err instanceof TechnologyError) {
+        throw err;
       }
-      throw new Error()
+      throw new Error();
     }
-} async update({id, title, deadline, user}: Params){
-  try{
-    const updateTechnology = await prisma.technology.update({
-      where: {
-        id,
-        userId:user.id
-      },
-      data: {
-        title,
-        deadline: new Date(deadline)
-      },
-    })
-    return updateTechnology
-  }catch(err){
-    console.log(err)
-    throw new Error()
   }
-} async updateMany({userId, id}: ParamsUserTech){
-  try{
-    const existingTechnology = await prisma.technology.findFirst({
-      where:{
-        userId: userId,
-        id: id
-      },
-    })
-    if (!existingTechnology) {
-      throw new TechnologyError('Technology does not exist');
+  async update({ id, title, deadline, user }: Params) {
+    try {
+      const updateTechnology = await prisma.technology.update({
+        where: {
+          id,
+          userId: user.id,
+        },
+        data: {
+          title,
+          deadline: new Date(deadline),
+        },
+      });
+      return updateTechnology;
+    } catch (err) {
+      console.log(err);
+      throw new Error();
     }
-    const updateTechnology = await prisma.technology.updateMany({
-      where: {
-        userId: userId,
-        id:id
-      },
-      data: {
-        studied: true,
-      },
-  })
-  return updateTechnology
-}catch(err){
-    console.log(err)
-    throw new Error()
   }
-} async delete({userId, id}: ParamsUserTech){
-  try{
-    const existingTechnology = await prisma.technology.findFirst({
-      where:{
-        userId: userId,
-        id: id
-      },
-    })
-    if (!existingTechnology) {
-      throw new TechnologyError('Technology does not exist');
+  async updateMany({ userId, id }: ParamsUserTech) {
+    try {
+      const existingTechnology = await prisma.technology.findFirst({
+        where: {
+          userId: userId,
+          id: id,
+        },
+      });
+      if (!existingTechnology) {
+        throw new TechnologyError("Technology does not exist");
+      }
+      const updateTechnology = await prisma.technology.updateMany({
+        where: {
+          userId: userId,
+          id: id,
+        },
+        data: {
+          studied: true,
+          
+        },
+      });
+      return updateTechnology;
+    } catch (err) {
+      console.log(err);
+      throw new Error();
     }
+  }
+  async delete({ userId, id }: ParamsUserTech) {
+    try {
+      const existingTechnology = await prisma.technology.findFirst({
+        where: {
+          userId: userId,
+          id: id,
+        },
+      });
+      if (!existingTechnology) {
+        throw new TechnologyError("Technology does not exist");
+      }
       const deleteTechnology = await prisma.technology.delete({
-      where: {
-        id,
-        userId
-      },
-    })
-    return deleteTechnology
-  }catch(err){
-    throw err
+        where: {
+          id,
+          userId,
+        },
+      });
+      return deleteTechnology;
+    } catch (err) {
+      throw err;
+    }
   }
-}}
+}
